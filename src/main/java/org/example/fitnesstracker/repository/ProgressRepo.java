3215ba4776ffd276cf67d705b390a7eb9eb3cb4d
Motsiiaka Denys <denzeml313@gmail.com>
2025-05-31 20:14:42 +0300

    rebranding and changing some of the project's goals

--- src/main/java/org/example/fitnesstracker/repository/ProgressRepo.java
+++
@@ -1,9 +0,0 @@
-package org.example.fitnesstracker.repository;
-
-import org.example.fitnesstracker.models.Progress;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProgressRepo extends JpaRepository<Progress, Long> {
-}
